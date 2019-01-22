ca5b48aa70e978c7e7f9968d694d3ec18cd887fa
Alexander Koval <kovalidis@gmail.com>
2019-01-22 17:40:45 +0200

    Throw exceptions on non-successful statuses

--- src/commonMain/kotlin/company/evo/kafka/connect/restclient/client.kt
+++ src/commonMain/kotlin/company/evo/kafka/connect/restclient/client.kt
@@ -1,17 +1,21 @@
 package company.evo.kafka.connect.restclient
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.ReceivePipelineException
 import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.features.HttpClientFeature
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
 import io.ktor.client.request.put
-import io.ktor.client.response.HttpResponse
+import io.ktor.client.response.HttpResponsePipeline
 import io.ktor.content.TextContent
-import io.ktor.http.ContentType
-import io.ktor.http.URLBuilder
 import io.ktor.http.clone
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.takeFrom
+import io.ktor.http.URLBuilder
+import io.ktor.util.AttributeKey
 
 import kotlinx.io.core.Closeable
 
@@ -19,9 +23,42 @@ import kotlinx.serialization.json.JSON
 import kotlinx.serialization.list
 import kotlinx.serialization.serializer
 
-class KafkaConnectRestException(
-    val statusCode: Int, val statusDescription: String
-) : Exception()
+open class KafkaConnectRestException(
+    val statusCode: Int, val statusDescription: String,
+    message: String = "Kafka connect server respond with an error"
+) : Exception("$message: $statusCode $statusDescription")
+{
+    open val isRetriable = false
+}
+
+class KafkaConnectRebalanceException(
+    statusCode: Int, statusDescription: String
+) : KafkaConnectRestException(
+    statusCode, statusDescription,
+    message = "Rebalance is in progress, try later"
+) {
+    override val isRetriable = true
+}
+
+internal class ExpectKafkaConnectSuccess {
+    companion object : HttpClientFeature<Unit, ExpectKafkaConnectSuccess> {
+        override val key = AttributeKey<ExpectKafkaConnectSuccess>("ExpectSuccess")
+
+        override fun prepare(block: Unit.() -> Unit) = ExpectKafkaConnectSuccess()
+
+        override fun install(feature: ExpectKafkaConnectSuccess, scope: HttpClient) {
+            scope.responsePipeline.intercept(HttpResponsePipeline.Receive) {
+                val status = context.response.status
+                if (status == HttpStatusCode.Conflict) {
+                    throw KafkaConnectRebalanceException(status.value, status.description)
+                }
+                if (status.value >= 300) {
+                    throw KafkaConnectRestException(status.value, status.description)
+                }
+            }
+        }
+    }
+}
 
 class KafkaConnectClient(
     url: String,
@@ -29,6 +66,8 @@ class KafkaConnectClient(
 ) : Closeable {
     val urlBuilder = URLBuilder(url)
     val httpClient = HttpClient(httpClientEngine) {
+        expectSuccess = false
+        install(ExpectKafkaConnectSuccess)
         install(JsonFeature) {
             serializer = KotlinxSerializer(JSON.nonstrict).apply {
                 register(ConnectInfo.serializer())
@@ -45,7 +84,7 @@ class KafkaConnectClient(
     }
 
     suspend fun info(): ConnectInfo {
-        return httpClient.get<ConnectInfo>(urlBuilder.build())
+        return httpClient.get(urlBuilder.build())
     }
 
     suspend fun connectors(): Iterable<String> {
@@ -58,22 +97,22 @@ class KafkaConnectClient(
         return JSON.nonstrict.parse(String.serializer().list, rawResp)
     }
 
-    suspend fun status(connectorName: String): ConnectorStatus? {
-        return httpClient.get<ConnectorStatus>(
+    suspend fun status(connectorName: String): ConnectorStatus {
+        return httpClient.get(
             urlBuilder.clone().path(listOf(CONNECTORS_ENDPOINT, connectorName, "status")).build()
         )
     }
 
     private suspend fun emptyPut(path: List<String>) {
-        val resp = httpClient.put<HttpResponse> {
-            url {
-                takeFrom(urlBuilder).path(path).build()
+        try {
+            httpClient.put<Unit> {
+                url {
+                    takeFrom(urlBuilder).path(path).build()
+                }
+                body = EMPTY_JSON_CONTENT
             }
-            body = EMPTY_JSON_CONTENT
-        }
-        // TODO Make a feature
-        if (resp.status.value >= 300) {
-            throw KafkaConnectRestException(resp.status.value, resp.status.description)
+        } catch (ex: ReceivePipelineException) {
+            throw ex.cause
         }
     }
 
--- src/commonTest/kotlin/company/evo/kafka/connect/restclient/ClientTests.kt
+++ src/commonTest/kotlin/company/evo/kafka/connect/restclient/ClientTests.kt
@@ -1,15 +1,17 @@
 package company.evo.kafka.connect.restclient
 
-import kotlinx.coroutines.runBlocking
-
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.response
+import io.ktor.client.engine.mock.responseError
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.headersOf
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+
+import kotlinx.coroutines.runBlocking
 
 class ClientTests {
 
@@ -26,7 +28,28 @@ class ClientTests {
         val info = client.info()
         assertEquals(info.version, "1.1")
         assertEquals(info.commit, "deadbeaf")
+    }
 
-        Unit
+    @Test
+    fun testPauseWhenRebalance() = runBlocking {
+        val client = KafkaConnectClient("http://localhost:8083", MockEngine {
+            responseError(
+                HttpStatusCode.Conflict,
+                headers = headersOf(
+                    HttpHeaders.ContentType, ContentType.Application.Json.toString()
+                )
+            )
+        })
+        val isExceptionRaised = try {
+            client.pause("test-connector")
+            false
+        } catch (ex: KafkaConnectRebalanceException) {
+            true
+        }
+        if (!isExceptionRaised) {
+            throw AssertionError(
+                "${KafkaConnectRebalanceException::class} exception was not raised"
+            )
+        }
     }
 }
