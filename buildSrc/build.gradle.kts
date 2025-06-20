754ca3b8d4a370cf2c939ad7f677e0ea795901c6
Alexander Koval <kovalidis@gmail.com>
2025-06-20 18:13:33 +0300

    Update nexus publication plugin: 1.1.0 -> 2.0.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -19,5 +19,5 @@ idea {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
-    implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
+    implementation("io.github.gradle-nexus:publish-plugin:2.0.0")
 }
